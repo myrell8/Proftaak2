9e1494717a1b3651ae4d407fbb6da337e7b0efea
Xragesunited <Remcovanderlinden78@gmail.com>
2018-12-03 16:01:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,30 +6,43 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<div class="container mt-4">
-
-		<div class="input-group">
+<nav class="navbar navbar-light bg-light navbar-expand">
+<a href="#" class="navbar-brand">NavBar</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+<ul class="navbar-nav mr-auto">
+<li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+<li class="nav-item"><a href="#" class="nav-link">Flights</a></li>
+<li class="nav-item"><a href="#" class="nav-link">News</a></li>
+</ul>
+<div class="pull-right">
+<div class="input-group">
 		  <input type="text" class="form-control" aria-label="Text input with dropdown button">
 		  <div class="input-group-append">
-		    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search options</button>
+		    <button class="btn btn-outline-success" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search options</button>
 		    <div class="dropdown-menu">
 		      <a class="dropdown-item" href="#">Filter op stad</a>
 		      <a class="dropdown-item" href="#">Filter op land</a>
 		      <a class="dropdown-item" href="#">Filter op IATA code</a>
 		    </div>
 		  </div>
+			</div>
 		</div>
-	</div>
 
-	<input id="userInput" type="text"  autofocus/>
-	<br>
-	<div class="test">
+</nav>
 
-	    <input id="scannerInput" type="text" value="barcodescan" autofocus/>
-	</div>
+
+	
 
 <div class="container">
- 	<div class="col-xs-12">
+ 	<div class="col-xs-12 mx-auto">
+	 <div class="col-sm-4 mx-auto m-5">
+	 <div class="d-flex flex-column justify-content-center mt-5">
+	 	<input id="userInput" class="form-control" type="text"  autofocus/>
+		<input id="scannerInput" class="form-control" type="text" value="barcodescan" autofocus/>
+	 </div>
+	</div>
     <div class="card-columns container-cards">
       <!-- Template will be displayed in here.. -->
     </div>
