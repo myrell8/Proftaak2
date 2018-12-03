8223b3cba2c5d01a92b63bc0adba5429d44fcd99
Xragesunited <Remcovanderlinden78@gmail.com>
2018-12-03 13:03:34 +0100

    update 1
    dyet

--- index.php
+++ index.php
@@ -1,10 +1,102 @@
+<!DOCTYPE html>
 <html>
 <head>
+	<title>Barcode Scanner</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-    
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    
-<script src="app.js"></script>
+	<div class="container mt-4">
+
+		<div class="input-group">
+		  <input type="text" class="form-control" aria-label="Text input with dropdown button">
+		  <div class="input-group-append">
+		    <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search options</button>
+		    <div class="dropdown-menu">
+		      <a class="dropdown-item" href="#">Filter op stad</a>
+		      <a class="dropdown-item" href="#">Filter op land</a>
+		      <a class="dropdown-item" href="#">Filter op IATA code</a>
+		    </div>
+		  </div>
+		</div>
+	</div>
+
+	<input id="userInput" type="text"  autofocus/>
+	<br>
+	<div class="test">
+
+	    <input id="scannerInput" type="text" value="barcodescan" autofocus/>
+	</div>
+
+
+    <div class="card-columns container-cards">
+      <!-- Template will be displayed in here.. -->
+    </div>
+
+    <template id="generate-flights">
+      {{#.}}
+      
+<div class="container">
+	<div class="card" style="width: 18rem;">
+	  <img class="card-img-top" src="img/flight1.gif" alt="Card image cap">
+	  <div class="card-body">
+	    <h5 class="card-title">Flight to: {{id}}</h5>
+	    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	    <!-- Button trigger modal -->
+		<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" value="{{}}">
+		  Generate barcode
+		</button>
+
+		<!-- Modal -->
+		<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        <svg id="barcode"></svg>
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary">Save changes</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+</div>
+
+{{/.}}
+</template>
 </body>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!-- Mustache JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.js"></script>
+
+<!-- Moment js  -->
+<script src="js/moment.min.js"></script>
+
+<!-- JsBarcode js -->
+<script src="js/JsBarcode.all.min.js"></script>
+
+<!-- JsBarcode js -->
+<script src="js/plugin.js"></script>
+
+<!-- kaas js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
+
+<!-- Custom js  -->
+<script src="js/main.js"></script>
 </html>
---
+++ js/main.js
@@ -0,0 +1,131 @@
+
+	$('#myModal').on('shown.bs.modal', function () {
+			$('#myInput').trigger('focus')
+		})
+
+$( "#btn-search-flight-id" ).click(function() {
+  
+  FlightSearch = $("#input-search-flight-id").val();
+
+  $.ajax({
+    url: "https://api.schiphol.nl/public-flights/destinations/"+FlightSearch+"?app_id=4a4a192f&app_key=32ec374a8485f67d0a7b8d362bb4228e",
+    dataType: "json",
+    headers:{"Accept": "application/json", "ResourceVersion": "v1"},
+    type: "GET",
+    contentType: "application/json",
+    success: function(data){
+
+	
+
+    	console.log(data);
+
+		// $.each(data, function (index, value) {
+		//   alert("dikke steen");
+		// });
+
+		var template = $("#generate-flights").html();
+
+		var renderTemplate = Mustache.render(template, data.flights);
+
+		mustache(data.flights, "#generate-flights", ".container-cards");
+
+    } 	
+})
+});
+
+dataCounter = 0;
+
+// Function to get all the coins which have been added to the portofolio
+function getFlightInformation() {
+
+	// Ajax function that gets data from get_coins_db.php
+    $.ajax({
+    url: "https://api.schiphol.nl/public-flights/flights?app_id=4a4a192f&app_key=32ec374a8485f67d0a7b8d362bb4228e&includedelays=false&page=0&sort=%2Bscheduletime",
+    dataType: "json",
+    headers:{"Accept": "application/json", "ResourceVersion": "v3"},
+    type: "GET",
+    contentType: "application/json",
+
+    success: function(data){
+
+		$('#myModal').on('shown.bs.modal', function () {
+			$('#myInput').trigger('focus')
+		})
+
+		console.log(data);
+
+    	flightId = data.flights[0].id;
+
+    	dataCounter++;
+
+        // amountOfFlights = data.flights.length;
+
+        // console.log("Amount of flights: " + amountOfFlights);
+
+		var template = $("#generate-flights").html();
+
+		var renderTemplate = Mustache.render(template, data.flights);
+
+		mustache(data.flights, "#generate-flights", ".container-cards");
+
+			JsBarcode("#barcode", flightId, {
+			width: 4,
+			height: 40,
+		});
+
+    }
+})
+}
+
+
+// Function that allows mustache to be loaded multiple times.
+function mustache(data, template, outerTemplate) {
+
+	if ($(outerTemplate).data('template')) {
+		var template = $(outerTemplate).data('template');
+	} else {
+		var template = $(template).html();
+		$(outerTemplate).data('template', template);
+	}
+
+	var renderTemplate = Mustache.render(template, data);
+
+	$(outerTemplate).html(renderTemplate);
+}
+
+$(document).ready(function(){
+
+
+
+	getFlightInformation();
+
+	$(document).scannerDetection({
+
+    //https://github.com/kabachello/jQuery-Scanner-Detection
+
+    timeBeforeScanTest: 200, // wait for the next character for upto 200ms
+    avgTimeByChar: 40, // it's not a barcode if a character takes longer than 100ms
+    preventDefault: true,
+
+    endChar: [13],
+    onComplete: function(barcode, qty){
+        validScan = true;
+
+
+        $('#scannerInput').val (barcode);
+
+    } // main callback function ,
+    ,
+    onError: function(string, qty) {
+
+        $('#userInput').val ($('#userInput').val()  + string);
+
+
+    }
+
+
+});
+
+		
+
+});
