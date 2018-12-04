a26c1497977bf7da569eea598074ec97c12c0fa9
Xragesunited <Remcovanderlinden78@gmail.com>
2018-12-04 14:07:31 +0100

    updates

--- index.php
+++ index.php
@@ -6,31 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-<nav class="navbar navbar-light bg-light navbar-expand">
-<a href="#" class="navbar-brand">NavBar</a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-<ul class="navbar-nav mr-auto">
-<li class="nav-item"><a href="#" class="nav-link">Home</a></li>
-<li class="nav-item"><a href="#" class="nav-link">Flights</a></li>
-<li class="nav-item"><a href="#" class="nav-link">News</a></li>
-</ul>
-<div class="pull-right">
-<div class="input-group">
-		  <input type="text" class="form-control" aria-label="Text input with dropdown button">
-		  <div class="input-group-append">
-		    <button class="btn btn-success" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search options <span class="caret"></span></button>
-		    <div class="dropdown-menu">
-		      <a class="dropdown-item" href="#">Filter op stad</a>
-		      <a class="dropdown-item" href="#">Filter op land</a>
-		      <a class="dropdown-item" href="#">Filter op IATA code</a>
-		    </div>
-		  </div>
-			</div>
-		</div>
-
-</nav>
+<?php include('includes/header.inc.php'); ?>
 
 
 	
@@ -82,7 +58,7 @@
     </div>
   </div>
 </div>
-
+<?php include('includes/footer.inc.php'); ?>
 </body>
 
 <!-- jQuery -->
