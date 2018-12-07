4bc219532e0e108345fb14ca18a7fd96a4631596
Unknown <myrell-9@outlook.com>
2018-12-07 10:07:34 +0100

    Structure updated

---
+++ about.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FlightCheck</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="css/anysearch.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-light bg-light navbar-expand">
+<a href="#" class="navbar-brand">NavBar</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+<ul class="navbar-nav mr-auto">
+<li class="nav-item"><a href="#" class="nav-link">Home</a></li>
+<li class="nav-item"><a href="#" class="nav-link">Flights</a></li>
+<li class="nav-item"><a href="#" class="nav-link">News</a></li>
+</ul>
+<div class="pull-right">
+<div class="input-group">
+		  <div class="input-group-append">
+		    <button class="btn btn-success" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search options <span class="caret"></span></button>
+		    <div class="dropdown-menu">
+		      <a class="dropdown-item" href="#">Filter op stad</a>
+		      <a class="dropdown-item" href="#">Filter op land</a>
+		      <a class="dropdown-item" href="#">Filter op IATA code</a>
+		    </div>
+		  </div>
+			</div>
+		</div>
+	</nav>
+
+
+
+
+</body>
+
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+
+<!-- Bootstrap -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!-- Mustache JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.3.0/mustache.js"></script>
+
+<!-- Moment js  -->
+<!-- <script src="js/moment.min.js"></script> -->
+
+<!-- JsBarcode js -->
+<script src="js/JsBarcode.all.min.js"></script>
+
+<!-- kaas js -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js"></script>
+
+<!-- Barcode Search Plugin -->
+<script src="js/anysearch.js"></script>
+
+<!-- Custom js  -->
+<script src="js/main.js"></script>
+</html>
