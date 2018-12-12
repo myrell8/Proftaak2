4601a0fbd6f3f96563b58181bfe93f423702b1a0
Unknown <myrell-9@outlook.com>
2018-12-12 14:56:17 +0100

    Logo text color change + navbar styling

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,4 @@
-<footer class="footer fixed-bottom d-flex justify-content-around align-items-center">
+<footer class="footer bg-dark fixed-bottom d-flex justify-content-around align-items-center">
     <span>FlightCheck</span>
-    <span>Work in progress</span>
     <span>ROC Ter-AA</span>
 </footer>
--- includes/header.inc.php
+++ includes/header.inc.php
@@ -1,23 +1,16 @@
-<nav class="navbar navbar-light bg-light navbar-expand">
-	<a href="index.php" class="navbar-brand"><img src="img/LogoSmall.png" height="46px"></a>
+<nav class="navbar navbar-dark bg-dark navbar-expand">
+	<a href="index.php" class="navbar-brand"><img src="img/LogoWhite.png" height="46px"></a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
 	</button>
-	<ul class="navbar-nav mr-auto">
+	<ul class="navbar-nav mr-auto d-flex justify-content-between ml-4">
 		<li class="nav-item"><a href="index.php" class="nav-link">Home</a></li>
 		<li class="nav-item"><a href="flights.php" class="nav-link">Flights</a></li>
 		<li class="nav-item"><a href="news.php" class="nav-link">News</a></li>
 		<li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
 		<li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
 	</ul>
-	<div class="pull-right">
-		<div class="input-group mb-3">
-
-			<input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
-			<div class="input-group-prepend">
-				<button class="btn btn-primary" type="button" id="button-addon1">Search</button>
-			</div>
-		</div>
-	</div>
-
+	<span class="navbar-text mr-2">
+      Work in progress
+    </span>
 </nav>
