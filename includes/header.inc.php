1152aedeea94cf20b6056c0a2b00d3b14f5879f2
Xragesunited <Remcovanderlinden78@gmail.com>
2018-12-14 08:55:49 +0100

    Auto stash before merge of "master" and "origin/master"

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -10,7 +10,30 @@
 		<li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
 		<li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
 	</ul>
+<<<<<<< Updated upstream
 	<span class="navbar-text mr-2">
       Work in progress
     </span>
+=======
+	<div class="pull-right">
+		<div class="input-group mb-3">
+
+			<input type="text" class="form-control" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
+			<div class="input-group-prepend">
+				<button class="btn btn-primary" type="button" id="button-addon1">Search</button>
+			</div>
+		</div>
+<!-- <div class="input-group mb-3">
+<div class="input-group-prepend">
+		  <input type="text" class="form-control" aria-label="Text input with dropdown button"> 
+		    <button class="btn btn-success" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Search options <span class="caret"></span></button>
+		    <div class="dropdown-menu">
+		      <a class="dropdown-item" href="#">Filter op stad</a>
+		      <a class="dropdown-item" href="#">Filter op land</a>
+		      <a class="dropdown-item" href="#">Filter op IATA code</a>
+		    </div>
+		  </div>
+		</div> -->
+	</div>
+>>>>>>> Stashed changes
 </nav>
