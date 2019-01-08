b631061725e6b23c1cbd4d253b61b721aa224403
Xragesunited <Remcovanderlinden78@gmail.com>
2019-01-08 15:07:26 +0100

    Update Mail js

---
+++ js/mail.js
@@ -0,0 +1,18 @@
+$(document).ready(() =>{
+    $("form").on("submit", () =>{
+      var form = $("form").serialize();
+      console.log(form);
+      $.ajax({
+        url: "includes/contactmail.php",
+        method: "POST",
+        data: form,
+        success: (data) =>{
+          alert(data);
+          if(data === "mail has been sent"){
+            window.location = "index.php"
+          } 
+        } 
+      });
+      return false;
+    });
+});
